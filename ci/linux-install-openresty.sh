09dfb7ed020bd20173238704c0b01d5404fc0873
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-11-06 19:28:47 +0530

    reference correct branch
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- ci/linux-install-openresty.sh
+++ ci/linux-install-openresty.sh
@@ -91,6 +91,6 @@ fi
 export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${openssl_prefix}/include"
 export ld_opt="-L${openssl_prefix}/lib -Wl,-rpath,${openssl_prefix}/lib"
 
-wget "https://raw.githubusercontent.com/api7/apisix-build-tools/master/build-apisix-runtime.sh"
+wget "https://raw.githubusercontent.com/api7/apisix-build-tools/openssl3/build-apisix-runtime.sh"
 chmod +x build-apisix-runtime.sh
 ./build-apisix-runtime.sh latest
