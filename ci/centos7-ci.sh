503079345a7867df69a3118adde711aeb3eb2df2
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-11-06 17:34:20 +0530

    change linker path
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- ci/centos7-ci.sh
+++ ci/centos7-ci.sh
@@ -28,7 +28,11 @@ install_openssl_3(){
     make -j $(nproc)
     make install
     OPENSSL_PREFIX=$(pwd)
-    export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
+    export LD_LIBRARY_PATH=$OPENSSL_PREFIX${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+    echo $OPENSSL_PREFIX
+    echo "content in $OPENSSL_PREFIX"
+    ls $OPENSSL_PREFIX
+    echo $OPENSSL_PREFIX > /etc/ld.so.conf.d/openssl3.conf
     ldconfig
     export openssl_prefix=$OPENSSL_PREFIX
     cd ..
